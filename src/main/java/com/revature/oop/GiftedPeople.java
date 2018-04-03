53c28f67f8d3bd300367eea6aff20a889ef67e90
Richard Park <hyunjae.v4@gmail.com>
2018-04-03 10:40:14 -0400

    Gifted dudes

---
+++ src/main/java/com/revature/oop/GiftedPeople.java
@@ -0,0 +1,17 @@
+package com.revature.oop;
+
+public class GiftedPeople {    //Naming convention is title-case or pascal-case. Each is uppercase
+
+	void giftedPowers() {    //This is camel-case. first undercase and second is uppercase
+		System.out.println("I got power!!!");
+	}
+	
+	void flaws() {
+		System.out.println("Gifted people have flaws.");
+	}
+	
+	void daemons() {
+		System.out.println("Gifted people have daemons");
+		
+	}
+}
