dcbc1606359e238cc112539630687121e52767fe
Austin D. Weed <adweed@terpmail.umd.edu>
2018-04-03 19:14:06 -0400

    First save of hw1 progress

---
+++ 1804_hw1/src/main/java/com/revature/corejava/AddSubtractDivideMultiply.java
@@ -0,0 +1,25 @@
+package com.revature.corejava;
+
+//Answer for hw1 question #1
+
+public class AddSubtractDivideMultiply {
+
+	public static void main(String[] args) {
+		
+		double a = 5;
+		double b = 2;
+
+		double add = a+b;
+		double subtract = a-b;
+		double divide = a/b;
+		double multiply = a*b;
+		
+		System.out.println("a = 5.0");
+		System.out.println("b = 2.0");
+		System.out.println("a + b = " + add);
+		System.out.println("a - b = " + subtract);
+		System.out.println("a/b = " + divide);
+		System.out.println("a*b = " + multiply);		
+	}
+
+}
