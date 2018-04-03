a3d899094c0e148a3d315ae1ce818394b434d486
Ian_Buitrago <kieldro@gmail.com>
2018-04-03 15:47:07 -0400

    hw indentation

--- com/revature/hello/Hw.java
+++ com/revature/hello/Hw.java
@@ -1,81 +1,80 @@
 // import java.util.*;
 
+package com.revature.hello;
+
 class Hw {
 	// class Inner{
-		
-		
+
 	// }
-	public static void main (String args[]){
+	public static void main(String args[]) {
 		// 1
-		int x = 1+2;
-		int y = 2-3;
-		int z = 3/5;
-		int w = 5*7;
+		int x = 1 + 2;
+		int y = 2 - 3;
+		int z = 3 / 5;
+		int w = 5 * 7;
 		System.out.println("1: finished.");
-		
+
 		// 2
 		int ans;
-		if ((x > y) && (x >z))
+		if ((x > y) && (x > z))
 			ans = x;
-		else if(y > z)
+		else if (y > z)
 			ans = y;
 		else
 			ans = z;
 		System.out.println("2:  " + ans);
-		
-		
+
 		System.out.println("3:");
 		boolean a = true, b = false;
 		System.out.println(!a);
-		System.out.println(a | b);		
+		System.out.println(a | b);
 		System.out.println((!a & b) | (a & !b));
-		
+
 		System.out.println("3:");
 		add(1, 2);
 		sub(1, 2);
 		mult(1, 2);
 		div(1, 2);
-		
+
 		// 5
-//		A.add(1,2);
-//		A.subtract(1,2);
-//		A.multiply(1,2);
-//		A.divide(1,2);
+		// A.add(1,2);
+		// A.subtract(1,2);
+		// A.multiply(1,2);
+		// A.divide(1,2);
 	}
-	
-	public static int add (int x, int y){
+
+	public static int add(int x, int y) {
 		return x + y;
 	}
-	
-	public static int sub (int x, int y){
+
+	public static int sub(int x, int y) {
 		return x - y;
 	}
-	
-	public static int mult (int x, int y){
+
+	public static int mult(int x, int y) {
 		return x * y;
 	}
-	
-	public static int div (int x, int y){
+
+	public static int div(int x, int y) {
 		return x / y;
 	}
 }
 
-
 class A {
-	
-	public static int add (int x, int y){
+
+	public static int add(int x, int y) {
 		return x + y;
 	}
-	
-	public static int sub (int x, int y){
+
+	public static int sub(int x, int y) {
 		return x - y;
 	}
-	
-	public static int mult (int x, int y){
+
+	public static int mult(int x, int y) {
 		return x * y;
 	}
-	
-	public static int div (int x, int y){
+
+	public static int div(int x, int y) {
 		return x / y;
 	}
 }
