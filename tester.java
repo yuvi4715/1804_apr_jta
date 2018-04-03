908fa41bd5395b78fb7447616803d1adce8c9671
Ian_Buitrago <kieldro@gmail.com>
2018-04-03 14:16:12 -0400

    new files; hw and tester

---
+++ tester.java
@@ -0,0 +1,22 @@
+import static org.junit.Assert.*;
+
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+
+public class tester {
+
+	@Before
+	public void setUp() throws Exception {
+	}
+
+	@After
+	public void tearDown() throws Exception {
+	}
+
+	@Test
+	public void test() {
+		fail("Not yet implemented");
+	}
+
+}
