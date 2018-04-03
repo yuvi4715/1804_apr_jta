47939cf0dbba6a205e12386f72881958a4edbe85
Kevin <kevinriveraut@gmail.com>
2018-04-03 14:19:18 -0400

    First commit, pushing project folder.

---
+++ Asssignment_Week1/src/core_Java/prob_1.java
@@ -0,0 +1,9 @@
+package core_Java;
+//Kevin Rivera - Revature
+//Assignment Week 1: Core Java - Problem 1.
+
+public class prob_1 {
+	public static void main(String[] args) {
+		System.out.println("Hello World!");
+	}
+}
