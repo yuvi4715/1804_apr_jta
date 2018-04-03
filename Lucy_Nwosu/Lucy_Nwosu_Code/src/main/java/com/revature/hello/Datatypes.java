ca56a2e0d99abe2fd57de619fe7456c6b2e4dc12
lucynwosu <lucynwosu123@gmail.com>
2018-04-02 18:51:09 -0700

    test

---
+++ Lucy_Nwosu/Lucy_Nwosu_Code/src/main/java/com/revature/hello/Datatypes.java
@@ -0,0 +1,32 @@
+package com.revature.hello;
+
+public final class Datatypes {
+	
+	int i;
+	char c;
+	double d;
+	float f;
+	long l;
+	short s;
+	byte b;
+	boolean bo;
+
+	public static void main(String[] args) {
+		Datatypes type=new Datatypes();
+		type.print();
+		
+	}
+		
+	void print() {
+		System.out.println("i :" + i);
+		System.out.println("c :" + c);
+		System.out.println("d :" + d);
+		System.out.println("f :" + f);
+		System.out.println("l :" + l);
+		System.out.println("s :" + s);
+		System.out.println("b :" + b);
+		System.out.println("bo :" + bo);
+
+	}
+
+}
