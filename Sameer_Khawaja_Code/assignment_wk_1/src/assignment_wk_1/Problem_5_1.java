9ac8938af0df72e7e6671a4a37273d58570dfbcf
Sameer Khawaja <sameerkhawaja01@gmail.com>
2018-04-04 16:35:57 -0400

    Add existing file

---
+++ Sameer_Khawaja_Code/assignment_wk_1/src/assignment_wk_1/Problem_5_1.java
@@ -0,0 +1,30 @@
+package assignment_wk_1;
+
+import java.util.Scanner;
+
+public class Problem_5_1 {
+
+	public static void main(String[] args) {
+		//create an instance of problem 5_2 which will contain methods
+		Problem_5_2 problem_5_2 = new Problem_5_2();
+		
+		
+		
+		Scanner reader = new Scanner(System.in);
+		System.out.println("Enter value 1");
+		float val_1 = reader.nextFloat();
+		System.out.println("Enter value 2");
+		float val_2 = reader.nextFloat();
+		
+		float result = problem_5_2.add(val_1, val_2);
+		System.out.println(val_1 + " + " + val_2 + " = " + result);
+		result = problem_5_2.subtract(val_1, val_2);
+		System.out.println(val_1 + " - " + val_2 + " = " + result);
+		result = problem_5_2.multiply(val_1, val_2);
+		System.out.println(val_1 + " * " + val_2 + " = " + result);
+		result = problem_5_2.divide(val_1, val_2);
+		System.out.println(val_1 + " / " + val_2 + " = " + result);
+		reader.close();
+	}
+
+}
