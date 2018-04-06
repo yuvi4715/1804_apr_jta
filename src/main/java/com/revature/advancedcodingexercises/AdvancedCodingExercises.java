3f571609f229c69c6d64557d5f96af0fea63fc17
Andrew McGovern <winedark.mcgovern@gmail.com>
2018-04-06 12:23:58 -0400

    finished isPrime

--- src/main/java/com/revature/advancedcodingexercises/AdvancedCodingExercises.java
+++ src/main/java/com/revature/advancedcodingexercises/AdvancedCodingExercises.java
@@ -1,5 +1,6 @@
 package com.revature.advancedcodingexercises;
 
+import java.util.ArrayList;
 import java.util.Calendar;
 import java.util.Date;
 
@@ -8,25 +9,45 @@ public class AdvancedCodingExercises {
 	public static void main(String[] args) {
 		String replacingChar = replaceChar("I ran I ran so far away", 'a', 'e');
 		System.out.println(replacingChar);
-		
-//		Calendar theCalendar = Calendar.getInstance();
-//		Date theDate = theCalendar.getTime();
-//		Date theDate2 = theCalendar.getTime();
-//		System.out.println(theDate.compareTo(new Date(98, 11, 20)));
-//		System.out.println(theDate.compareTo(theDate));
-		
+
 		Date myBirthday = new Date(88, 4, 5);
 		Date bd = Calendar.getInstance().getTime();
 		System.out.println(birthday(myBirthday));
-		System.out.println(fibonacci(12));
-		System.out.println(fibonacciRecursive(12));
-
+		printFibonacci();
+		fizzBuzz();
+		
+		// Question 42.
+		Employee employee2 = new Employee("Sam", "Marketing", 28);
+		Employee employee1 = new Employee("John", "Engineering", 30);
+		Employee employee3 = new Employee("Pam", "HR", 25);
+		ArrayList<Employee> al = new ArrayList<>();  
+		al.add(employee1);
+		al.add(employee2);
+		al.add(employee3);
+		al.sort((a, b) -> { return a.getName().compareTo(b.getName()); });
+		al.sort((a, b) -> { return a.getDepartment().compareTo(b.getDepartment()); });
+		al.sort((a, b) -> { return new Integer(a.getAge()).compareTo(new Integer(b.getAge())); });
+		for (Employee e : al) {
+			System.out.println(e);
+		}
+		
+		System.out.println(isArmstrong(408));
+		System.out.println(reverse("evol"));
+		
+		for (int i = 2 ; i < 50 ; i++) {
+			if (isPrime(i)) {
+				System.out.println(i);
+			}
+		}
+			
 	}
 	
+	// Question 38
 	static String replaceChar(String string, char char1, char char2) {
 		return string.replace(char1, char2);
 	}
 
+	// Question 39
 	static int birthday(Date birthdayDate) {
 		Calendar theCalendar = Calendar.getInstance();
 		Date now = theCalendar.getTime();
@@ -47,10 +68,15 @@ public class AdvancedCodingExercises {
 		return age;
 	}
 	
+	// Question 40
 	static int fibonacci(int n) {
 		int a = 0;
 		int b = 1;
 		int c = 0;	
+		
+		if (n == 1) {
+			return 1;
+		}
 		
 		for (int i = 2 ; i <= n ; i++) {
 			c = a + b;
@@ -71,7 +97,60 @@ public class AdvancedCodingExercises {
 	    	return fibonacciRecursive(n - 1) + fibonacciRecursive(n - 2);
 	    }
 	}
-	    	
+	
+	static void printFibonacci() {
+		for (int i = 0 ; i < 100 ; i++) {
+			System.out.println(fibonacci(i));
+		}
+	}
+	
+	// Question 41
+	static void fizzBuzz() {
+		for (int i = 1 ; i <= 50 ; i++) {
+			if (i % 3 == 0 && i % 5 == 0) {
+				System.out.println("FizzBuzz");
+			} else if (i % 3 == 0) {
+				System.out.println("Fizz");
+			} else if (i % 5 == 0) {
+				System.out.println("Buzz");
+			} else {
+				System.out.println(i);
+			}
+		}
+	}
+	  
+	// Question 43
+	static boolean isArmstrong(int n) {
+		int num1 = (int) Math.floor(n / 100);
+		int num2 = (int) Math.floor((n % 100) / 10);
+		int num3 = n % 10;
+	
+		return n == ( Math.pow(num1, 3) + Math.pow(num2, 3) + Math.pow(num3, 3) );
+	}
+	
+	// Question 44
+	static String reverse(String str) {
+		StringBuilder sb = new StringBuilder("");
+		for (int i = str.length() - 1 ; i >= 0 ; i--) {
+			sb.append(str.charAt(i));
+		}
+		return sb.toString();
+	}
+	
+	// Question 45
+	static boolean isPrime(int n) {
+		if (n == 2) {
+			return true;
+		}
+		
+		for (int i = 2 ; i < n ; i++) {
+			if (n % i == 0) {
+				return false;
+			}
+		}
+		
+		return true;
+	}
 	    	
 }
 					
---
+++ src/main/java/com/revature/advancedcodingexercises/Employee.java
@@ -0,0 +1,44 @@
+package com.revature.advancedcodingexercises;
+
+public class Employee {
+	private String name;
+	private String department;
+	private int age;
+	
+	public Employee(String name, String department, int age) {
+		super();
+		this.name = name;
+		this.department = department;
+		this.age = age;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public String getDepartment() {
+		return department;
+	}
+
+	public void setDepartment(String department) {
+		this.department = department;
+	}
+
+	public int getAge() {
+		return age;
+	}
+
+	public void setAge(int age) {
+		this.age = age;
+	}
+
+	@Override
+	public String toString() {
+		return "Employee [name=" + name + ", department=" + department + ", age=" + age + "]";
+	}
+	
+}
