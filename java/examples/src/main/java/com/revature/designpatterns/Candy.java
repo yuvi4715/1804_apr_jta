eb1cba5f5647f8443bce397a604d39958206cebe
Yuvi <yuvarajd@revature.com>
2018-04-06 16:38:37 -0400

    day 5

---
+++ java/examples/src/main/java/com/revature/designpatterns/Candy.java
@@ -0,0 +1,10 @@
+package com.revature.designpatterns;
+
+public class Candy implements Sweets {
+
+	@Override
+	public void bake() {
+		System.out.println("candy is baked and ready to be eaten");
+	}
+
+}
