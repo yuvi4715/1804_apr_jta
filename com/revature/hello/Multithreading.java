3d51defa02f7956afc0835fdf8a800a931874e5d
Ian_Buitrago <kieldro@gmail.com>
2018-04-07 02:19:51 -0400

    multithreading done

--- com/revature/hello/Multithreading.java
+++ com/revature/hello/Multithreading.java
@@ -1,37 +1,77 @@
 package com.revature.hello;
 
-public class Multithreading extends Thread{
+import java.util.concurrent.TimeUnit;
+
+public class Multithreading extends Thread {
 	// Multithreading
 	// Write a method that stores an integer. Each time the method is called,
 	// increment the integer by 1. Create three threads that call this method 1000
 	// times each, storing the returned result in their own set. Print out each
 	// thread’s set.
-	static int i = 0;
-	public static void main(String args[]) {
-		Thread t = new Multithreading();
-		t.start();
-		(new Multithreading()).start();
-		(new Multithreading()).start();
+	static int num = 0;
+	static boolean sync = false;
+
+	public static void main(String args[]) throws Exception {
+		int size = 3;
+		Thread[] tArray = new Thread[size];
+		System.out.println("unsychronized:");
+		for (int i = 0; i < size; ++i) {
+			tArray[i] = new Multithreading();
+		}
+		for (int i = 0; i < size; ++i) {
+			tArray[i].run();
+		}
+		
+//		TimeUnit.SECONDS.sleep(1);
+		for (int i = 0; i < size; ++i) {
+			tArray[i].join();
+		}
+
+		System.out.println("sychronized:");
+		Multithreading.num = 0;
+		Multithreading.sync = true;
+		for (int i = 0; i < size; ++i) {
+			Thread t = new Multithreading();
+			t.run();
+		}
+
+		// Review thread-specific methods: sleep(), wait(), yield(), suspend(), stop(),
+		// resume(), interrupt(), notify(), notifyAll()
+		Thread test = new Multithreading();
+		test.start();
+		test.sleep(10);
+//		test.wait();
+//		test.notify();
+//		test.yield();
+//		test.interrupt();
+//		test.notifyAll();
+		
+		// depreciated methods
+//		test.suspend();
+//		test.stop();
+//		test.resume();
 	}
 
 	public void run() {
 		super.run();
-		System.out.println("I'm doing stuff inside a thread!");
+		// System.out.println("I'm doing stuff inside a thread!");
 		int result = 0;
-		for (int i = 0; i < 1000; ++i) {
-			result = foo();
+		for (int i = 0; i < 100; ++i) {
+			result = sync ? bar() : foo();
 		}
 		System.out.println("result: " + result);
 	}
 
-	public synchronized int foo() {
-		return ++i;
+	public int foo() {
+//		System.out.println("foo()");
+		return ++num;
 	}
 
 	// Synchronize the method that stores and increments the integer. Execute your
 	// threads again.
-	// Review thread-specific methods: sleep(), wait(), yield(), suspend(), stop(),
-	// resume(), interrupt(), notify(), notifyAll()
+	public synchronized int bar() {
+		return ++num;
+	}
 
 }
 /*
