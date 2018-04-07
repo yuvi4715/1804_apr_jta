fc78ace4d74b97e3c7a723a2b1aa7e3c5d55bb24
Ian_Buitrago <kieldro@gmail.com>
2018-04-07 18:56:19 -0400

    Calendar complete.

--- com/revature/hello/AdvCodingEx.java
+++ com/revature/hello/AdvCodingEx.java
@@ -1,6 +1,5 @@
 package com.revature.hello;
 
-import java.time.LocalDate;
 import java.util.Calendar;
 
 public class AdvCodingEx {
@@ -15,9 +14,9 @@ public class AdvCodingEx {
 
 		// Write a method that takes a date of birth via Calendar object and returns
 		// your age. If it is your birthday, print the Happy Birthday song!
-		Calendar c = Calendar.getInstance();
-		c.set(1984, 1, 4);
-		age(c);
+		Calendar birthday = Calendar.getInstance();
+		birthday.set(1984, 1, 4); // feb 4 1984
+		System.out.println("age: " + age(birthday));
 
 	}
 
@@ -25,10 +24,31 @@ public class AdvCodingEx {
 		return s.replaceFirst(String.valueOf(c), String.valueOf(sub));
 	}
 
-	public static int age(Calendar c) {
-		System.out.println(c.getTime());
-		System.out.println(LocalDate.now());
-		return 0;
+	public static int age(Calendar birthday) {
+		Calendar today = Calendar.getInstance();
+//		today.set(2018, 1, 4);
+		int age = today.get(Calendar.YEAR) - birthday.get(Calendar.YEAR);
+
+//		System.out.println(today.getTime());
+//		System.out.println(birthday.getTime());
+//		System.out.println(today.after(birthday));
+//		System.out.println(today.get(Calendar.YEAR));
+//		System.out.println(birthday.get(Calendar.YEAR));
+//		System.out.println(birthday.get(Calendar.MONTH));
+
+		if (today.get(Calendar.MONTH) == birthday.get(Calendar.MONTH)
+				&& today.get(Calendar.DATE) == birthday.get(Calendar.DATE)) {
+			System.out.println("HAPPY BIRTHDAY!!");
+			return age;
+		}
+		
+		// no birthday this year yet
+		if (today.get(Calendar.MONTH) < birthday.get(Calendar.MONTH)
+			|| today.get(Calendar.MONTH) == birthday.get(Calendar.MONTH)
+				&& today.get(Calendar.DATE) < birthday.get(Calendar.DATE))
+			--age;
+		return age;
+
 	}
 
 }
