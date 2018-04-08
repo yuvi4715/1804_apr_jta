e2a16dbd0f635a6ff6b1dae985758dc3a2d5c8a9
Ian_Buitrago <kieldro@gmail.com>
2018-04-07 20:27:16 -0400

    fib done

--- com/revature/hello/Fibonacci.java
+++ com/revature/hello/Fibonacci.java
@@ -5,20 +5,31 @@ public class Fibonacci {
 	// iteration. Rewrite using recursion.
 
 	public static void main(String[] args) {
-		fibIterative();
+		fibIterative(100);
 		
+		System.out.println("recursive : " + fibRecursive(10));
+
 	}
-	
-	public static void fibIterative() {
+
+	public static void fibIterative(int n) {
 		System.out.println("Fiboniacci sequence calculated iteratively: ");
 
-		long former = 0;
-		long latter = 1;
-		for (int i = 0; i < 90; ++i) {
-			long num = latter + former;
+		double former = 0;
+		double latter = 1;
+		for (int i = 0; i < n; ++i) {
+			double num = latter + former;
 			former = latter;
 			latter = num;
 			System.out.println(num);
+		}
+	}
+
+
+	public static double fibRecursive(int n) {
+		if (n <= 1) {
+			return 1;
+		} else {
+			return fibRecursive(n - 1) + fibRecursive(n - 2);
 		}
 	}
 }
