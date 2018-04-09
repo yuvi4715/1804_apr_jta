6e1f4db370ccef73c483a9b8b952344ee6d94967
Jackie Chen <jackiechen5443@gmail.com>
2018-04-09 10:58:40 -0400

    Added logger

--- Cheng_Chen-code/Project0/src/Bank/BankMain.java
+++ Cheng_Chen-code/Project0/src/Bank/BankMain.java
@@ -1,10 +1,15 @@
 package Bank;
 
+import org.apache.log4j.Logger;
+
 public class BankMain {
 
+	final static Logger log = Logger.getLogger(BankMain.class);
+	
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-
+		//log usage for later
+		//log.info("INFO SHOULD BE PRINTED.");
 	}
 
 }
