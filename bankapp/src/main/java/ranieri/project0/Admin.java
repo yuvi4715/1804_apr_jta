13803482772098a3f8f79d59832f0face2259385
Adam Ranieri <adaman94@gmail.com>
2018-04-12 19:16:13 -0400

    Add files via upload

--- bankapp/src/main/java/ranieri/project0/Admin.java
+++ bankapp/src/main/java/ranieri/project0/Admin.java
@@ -13,14 +13,33 @@ public class Admin {
 		this.username = username;
 		this.password = password;
 	}
+	
+	public void getAllUnauthorizedUsers() {
+		// method that shows all unauthorized users
+		
+	}
 
 	public void authorizeUser(Customer customer) {
+		//
+		
 		customer.setAuthorized(true);		
 	}
 	
 	public void createAdmin(String username, String password) {
 		
+		// method that creates a sql insert for new admin
+		
 	}
+
+	public String getUsername() {
+		return username;
+	}
+
+	public void setUsername(String username) {
+		this.username = username;
+	}
+	
+	
 	
 	
 
