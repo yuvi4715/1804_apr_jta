c79fea86dc4d1c3b69f2a46d0b235ec5ba93c409
KevinRiveraUT <kevinriveraut@gmail.com>
2018-04-24 16:55:01 -0400

    Added bunch of files to gitignore. Also progress on ERS.

---
+++ ERS/src/main/java/com/revature/ers/service/ERS_Service.java
@@ -0,0 +1,43 @@
+package com.revature.ers.service;
+
+import java.util.List;
+
+import com.revature.ers.dao.*;
+import com.revature.ers.model.ERS_User;
+import com.revature.ers.model.Request;
+
+public class ERS_Service{
+    private static ERSDAO dao = new ERSDAOImpl();
+
+    public boolean insert_user(ERS_User user){
+        return dao.insert_user(user);
+    }
+
+    public boolean insert_request(Request request){
+        return dao.insert_request(request);
+    }
+
+    public boolean modify_request(Request request){
+        return dao.modify_request(request);
+    }
+
+    public boolean modify_user(ERS_User user){
+        return dao.modify_user(user);
+    }
+
+    public List<Request> emp_view_certain_requests(ERS_User user, String status){
+        return dao.emp_view_certain_requests(user, status);
+    }
+
+    public List<Request> man_view_all_requests(){
+        return dao.man_view_all_requests();
+    }
+
+    public List<Request> man_view_certain_requests(String status){
+        return dao.man_view_certain_requests(status);
+    }
+
+    public List<Request> man_view_emp_requests(int requester){
+        return dao.man_view_emp_requests(requester);
+    }
+}
