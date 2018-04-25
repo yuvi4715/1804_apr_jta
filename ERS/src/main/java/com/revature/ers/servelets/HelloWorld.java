922adcb2148d679b8bca3f41206c789cb14b84eb
KevinRiveraUT <kevinriveraut@gmail.com>
2018-04-25 11:17:54 -0400

    Testing RequestDispatcher's forward and include

--- ERS/src/main/java/com/revature/ers/servelets/HelloWorld.java
+++ ERS/src/main/java/com/revature/ers/servelets/HelloWorld.java
@@ -1,42 +1,41 @@
 package com.revature.ers.servelets;
 
 import java.io.IOException;
+
+import javax.servlet.RequestDispatcher;
 import javax.servlet.ServletConfig;
 import javax.servlet.ServletException;
+import javax.servlet.ServletRequest;
+import javax.servlet.ServletResponse;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
 public class HelloWorld extends HttpServlet {
-	private static final long serialVersionUID = 1L;
-       
+	private static final long serialVersionUID = 1L; 
     
     public HelloWorld() {
         super();
-        // TODO Auto-generated constructor stub
     }
 
-	
 	//public void init(ServletConfig config) throws ServletException {
-		// TODO Auto-generated method stub
+		
 	//}
 
-	
 	//public void destroy() {
-		// TODO Auto-generated method stub
+		
 	//}
 
-	
 	//protected void service(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		// TODO Auto-generated method stub
+		
 	//}
 
-	
 	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		response.getWriter().append("Served at as a: G E T");
+		//response.getWriter().append("Served at as a: G E T");
+		RequestDispatcher rd = request.getRequestDispatcher("Test.dotoo");
+		rd.forward(request, response);
 	}
 
-	
 	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 		response.getWriter().append("Served as a: P O S T");
 	}
