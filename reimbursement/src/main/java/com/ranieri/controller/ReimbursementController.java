73fc2412d7d3187be85bcc54bf2965735e408f1c
Adam Ranieri <adaman94@gmail.com>
2018-04-27 13:45:08 -0400

    Add files via upload

---
+++ reimbursement/src/main/java/com/ranieri/controller/ReimbursementController.java
@@ -0,0 +1,58 @@
+package com.ranieri.controller;
+
+import java.util.List;
+
+import javax.servlet.http.HttpServletRequest;
+
+import com.ranieri.model.Request;
+import com.ranieri.service.RequestService;
+
+public class ReimbursementController {
+	
+	
+	
+public static String doRequest(HttpServletRequest req) {
+	
+	
+	String email = req.getParameter("email");
+	String reason = req.getParameter("reason");
+	String amount = req.getParameter("amount");
+	String image = req.getParameter("image");
+	
+	System.out.println("before the create");
+	RequestService.createRequest(email, reason, Double.parseDouble(amount), image);
+	System.out.println("after the create");
+	
+	
+	return "created request";
+	
+}
+
+public static String getEmployeeReimbursemt(String email) {
+	
+	List<Request> list = RequestService.getAllRequests();
+	
+	String data ="";
+	for(Request x : list) {
+		
+		if(x.getEmail().equals(email)){
+		data= data +x +"\n";
+		}
+		
+	}
+	
+	return data;
+}
+	
+	
+	
+	
+	public static void main(String[] args) {
+				
+//		String test = ReimbursementController.getEmployeeReimbursemt("bruce@wayne.com");		
+//		System.out.println(test);
+//		ReimbursementController.doRequest("kim@wayne.com", "mod pizza", "180", "cracked screen");	
+		
+	}
+
+}
--- reimbursement/src/main/java/com/ranieri/servlet/RequestHelper.java
+++ reimbursement/src/main/java/com/ranieri/servlet/RequestHelper.java
@@ -5,6 +5,7 @@ import javax.servlet.http.HttpServletRequest;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.ranieri.controller.EmployeeController;
 import com.ranieri.controller.ManagerController;
+import com.ranieri.controller.ReimbursementController;
 import com.ranieri.model.Employee;
 import com.ranieri.service.EmployeeService;
 
@@ -23,7 +24,10 @@ public static String process(HttpServletRequest req) {
 		
 		case "/reimbursement/getAllReimbursements.do":
 			return ManagerController.showAllRemibursemets(req);
-				
+		
+		case "/reimbursement/createRequest.do":
+			return ReimbursementController.doRequest(req);
+			
 		default:
 				return "unable to reach database";
 		
